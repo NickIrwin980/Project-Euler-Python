6c606bea3def294373a68e1b56c7edf0ebddfeb3
NickIrwin980 <nick980@wowway.com>
2015-11-04 18:29:03 -0500

    Update PE1Multiples.py

--- PE1Multiples.py
+++ PE1Multiples.py
@@ -2,18 +2,19 @@
 """
 Problem 1:
 
-* Add all the natural numbers below 1000 that are multiples of 3 or 5.
+* Find the sum of all the natural numbers below 1000 that are multiples
+  of 3 or 5.
 
 Modifications:
 
 * The two factors and highest number are variables and can be changed.
 
 * An array is used instead of a generator so that the
-contents may be viewed or altered at any time.
+  contents may be viewed or altered at any time.
 
 """
 __author__ = "Nick Irwin"
-__version__ = "1.0"
+__version__ = "1.0.1"
 __email__ = "nick980@wowway.com"
 
 def sumMultiples(factor1, factor2, low, high):
